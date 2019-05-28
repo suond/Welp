61c9d353f28e77bcf674e737e933eb562afd7d90
david <dsuon04@gmail.com>
2019-05-28 08:48:03 -0700

    fixed seed minor styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'open-uri'
+require 'open-uri'
 
 
 dummyUser = User.create!(fName: "Jimmy", lName: "Kudos", zipcode: "12345", password: "123456", email: "fake@this.com" )
-# dummyAvatar = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/dummy_avatar.jpg')
-# dummyUser.avatar.attach(io: dummyAvatar, filename: 'avatar.jpg')
+dummyAvatar = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/jk.png')
+dummyUser.avatar.attach(io: dummyAvatar, filename: 'avatar.jpg')
 
 
 restBurger = Restaurant.create!(owner_id: 1, location_id: 1, name: "Out-n-In", opening_time:"10:00", closing_time:"17:00", address:"1234 not a real address", city: "Stockton", state:"CA", zipcode: "12345")
-# burgerPhoto1 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/burger.jpg')
-# restBurger.photos.attach(io: burgerPhoto1, filename: 'burger1.jpg')
-# burgerPhoto2 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/burger2.jpg')
-# restBurger.photos.attach(io: burgerPhoto2, filename: 'burger2.jpg')
+burgerPhoto1 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/burger.jpg')
+restBurger.photos.attach(io: burgerPhoto1, filename: 'burger1.jpg')
+burgerPhoto2 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/burger2.jpg')
+restBurger.photos.attach(io: burgerPhoto2, filename: 'burger2.jpg')
 
 restPho = Restaurant.create!(owner_id: 1, location_id: 1, name: "Pho-King", opening_time:"11:00 am", closing_time:"8:00 pm", address:"1234 not a real address", city: "Sacramento", state:"CA", zipcode: "12345")
-# pho1 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/pho.jpg')
-# restPho.photos.attach(io: pho1, filename: "pho1.jpg")
+pho1 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/pho.jpg')
+restPho.photos.attach(io: pho1, filename: "pho1.jpg")
 
 Location.create!(lat: 0.0, lng: 0.0, restaurant_id: 1)
