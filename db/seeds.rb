5c90c186cf048940b50e4532378f289c8f8f28ce
david <dsuon04@gmail.com>
2019-05-29 14:13:35 -0700

    changes to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ dummyAvatar = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/jk.png')
 dummyUser.avatar.attach(io: dummyAvatar, filename: 'avatar.jpg')
 
 
-restBurger = Restaurant.create!(owner_id: 1, location_id: 1, name: "Out-n-In", opening_time:"10:00", closing_time:"17:00", address:"1234 not a real address", city: "Stockton", state:"CA", zipcode: "12345")
+restBurger = Restaurant.create!(owner_id: 1, location_id: 1, name: "Out-n-In", opening_time:"10:00", closing_time:"17:00", address:"1234 not a real address", city: "Stockton", state:"CA", zipcode: "12345", phone_number: "123-456-7890", website: "https://www.google.com")
 burgerPhoto1 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/burger.jpg')
 restBurger.photos.attach(io: burgerPhoto1, filename: 'burger1.jpg')
 burgerPhoto2 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/burger2.jpg')
 restBurger.photos.attach(io: burgerPhoto2, filename: 'burger2.jpg')
 
-restPho = Restaurant.create!(owner_id: 1, location_id: 1, name: "Pho-King", opening_time:"11:00 am", closing_time:"8:00 pm", address:"1234 not a real address", city: "Sacramento", state:"CA", zipcode: "12345")
+restPho = Restaurant.create!(owner_id: 1, location_id: 1, name: "Pho-King", opening_time:"11:00 am", closing_time:"8:00 pm", address:"1234 not a real address", city: "Sacramento", state:"CA", zipcode: "12345", phone_number: "987-654-3210", website: "https://phoking.com" )
 pho1 = open('https://s3-us-west-1.amazonaws.com/aa-welp-dev/pho.jpg')
 restPho.photos.attach(io: pho1, filename: "pho1.jpg")
 
